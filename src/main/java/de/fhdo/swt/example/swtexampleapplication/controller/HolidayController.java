8af3bc9024e7e1ea1fee0d483b41e4d7ac9b2c5e
Jan Preuschoff <preuschoffjan@gmail.com>
2020-06-22 13:48:01 +0200

    Replaced test data classes with repositories

--- src/main/java/de/fhdo/swt/example/swtexampleapplication/controller/HolidayController.java
+++ src/main/java/de/fhdo/swt/example/swtexampleapplication/controller/HolidayController.java
@@ -1,18 +1,23 @@
 package de.fhdo.swt.example.swtexampleapplication.controller;
 
 import de.fhdo.swt.example.swtexampleapplication.entity.Holiday;
-import de.fhdo.swt.example.swtexampleapplication.entity.Holidays;
-import de.fhdo.swt.example.swtexampleapplication.entity.RecommendedHotels;
+import de.fhdo.swt.example.swtexampleapplication.service.HolidayService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HolidayController {
+    @Autowired
+    private HolidayService service;
+
     @GetMapping("/")
     public String showHolidaysForm(Holiday holiday, Model model){
-        model.addAttribute("holidays", new Holidays());
-        model.addAttribute("recommendations", new RecommendedHotels());
+        Iterable<Holiday> data = service.findAll();
+        model.addAttribute("holidays", data);
+        model.addAttribute("recommendations", data);
         return "index";
     }
 }
