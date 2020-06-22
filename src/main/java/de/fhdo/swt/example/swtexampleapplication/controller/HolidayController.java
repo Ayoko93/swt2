743795c967cbe2cb40e753d30c55fd0ea9b0b303
Adrian <Adrian.Juettner@live.de>
2020-06-22 14:15:24 +0200

    Changed HolidayController, holiday-detail.html

--- src/main/java/de/fhdo/swt/example/swtexampleapplication/controller/HolidayController.java
+++ src/main/java/de/fhdo/swt/example/swtexampleapplication/controller/HolidayController.java
@@ -18,16 +18,15 @@ public class HolidayController {
     public String showHolidaysForm(Holiday holiday, Model model){
         Iterable<Holiday> data = service.findAll();
         model.addAttribute("holidays", data);
-        model.addAttribute("recommendations", data);
+        model.addAttribute("recommended", data);
         return "index";
     }
 
     @GetMapping("/holiday/{id}")
     public String showHolidaysForm(Holiday holiday, Model model, @PathVariable long id){
-        RecommendedHotels hotels = new RecommendedHotels();
-        Holiday h = hotels.stream().filter((h1) -> h1.getId() == id).findFirst().get();
+        Holiday data = service.find(id);
 
-        model.addAttribute("holiday", h);
+        model.addAttribute("holiday", data);
         return "holiday-detail";
     }
 
