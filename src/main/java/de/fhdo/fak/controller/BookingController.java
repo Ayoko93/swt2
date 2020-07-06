d7ef7b650b4d43b1bd2594184f0a524445fceabb
T-Power <50549020+T-Power@users.noreply.github.com>
2020-07-06 13:47:19 +0200

    added booking html and controller

---
+++ src/main/java/de/fhdo/fak/controller/BookingController.java
@@ -0,0 +1,22 @@
+package de.fhdo.fak.controller;
+
+import de.fhdo.fak.repository.SupportTicketRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookingController {
+
+
+    @GetMapping("/bookings")
+    public String showSupportTicketsForm(Model model){
+
+
+
+        return "bookings";
+    }
+
+
+}
